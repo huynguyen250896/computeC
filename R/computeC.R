72737d6d5797890e35161caf745a2e6cb3f58f3c
Quang-Huy Nguyen <30990121+huynguyen250896@users.noreply.github.com>
2021-01-20 00:28:34 +0700

    Update computeC.R

--- R/computeC.R
+++ R/computeC.R
@@ -1,16 +1,30 @@
-#' @title Automatically compute the  correlation between genes and clinical features
+#' @title Automatically perform correlation analyses between individual genes and each clinical feature of your interest
 #'
-#' @description Automatically analyze the correlation between individual genes and clinical features of interest
+#' @description Automatically perform correlation analyses between individual genes and each clinical feature of your interest
 #'
-#' @param data,var,x
+#' @docType package
+#'
+#' @author Quang-Huy Nguyen
+#'
+#' @usage computeC(data,clinical,col, methodCC)
+#'
+#' @param data data frame or matrix. The data includes its rows are samples and its columns are genes.
+#'
+#' @param clinical data frame. The data includes its rows are samples and its columns are genes. Note that
+#' samples are also included in rows of \code{data} and in the same order.
+#'
+#' @param col character. name of any columns in \code{clinical}. This must be a clinical feature that you are interest.
+#'
+#' @param methodCC character. correlation method. This must be a clinical feature that you are interest. Allowed values 
+#' are \code{spearman} (default), \code{pearson}, \code{kendall}.
 #'
 #' @return NULL
 #'
-#' @examples computeC(cor,feature,"lymph")
+#' @examples computeC(data = exp, clinical = cli, col = "lymph")
 #'
 #' @export
 
-computeC = function(data,var,x, methodCC = "spearman")
+computeC = function(data,clinical,col, methodCC = "spearman")
 {
   #library
   library(tidyr)
@@ -19,29 +33,37 @@ computeC = function(data,var,x, methodCC = "spearman")
   
   #missing input
   if(missing(data)){
-    stop("Error: omics input is missing \n")
+    stop("Error: omics input is missing. \n")
   }
-
-  if(missing(var)){
-    stop("Error: clinical data is missing \n")
+  
+  if(missing(clinical)){
+    stop("Error: clinical data is missing. \n")
   }
-
-  if(missing(x)){
-    stop("Error: clinical feature column in clinical data is missing \n")
+  
+  if(missing(col)){
+    stop("Error: clinical feature column in clinical data is missing. \n")
+  }
+  
+  if(!(col %in% colnames(cli))){
+    stop("Error: clinical feature being selected is not included in any columns of clinical data. \n")
+  }
+  
+  if(all(rownames(data) != rownames(clinical))){
+    stop("Error: Please make sure samples sharing between input data and clinical data are included at rows of the two and in the same order.")
   }
   
   #define the computeQ function may adjust gained P-values following Benjamini-Hochberg FDR
   computeQ <- function(x)
   {
-  (x$P.value*nrow(x))/(x$rank)
+    (x$P.value*nrow(x))/(x$rank)
   }
   
   #implementation
-  cc1 <- data.frame(name=paste("Site", 1:ncol(data)),CC=NA ,P.value=NA)
+  cc1 <- data.frame(My_name_is=paste("Huy", 1:ncol(data)),CC=NA ,P.value=NA)
   estimates = numeric(ncol(data))
   pvalues = numeric(ncol(data))
   for (i in c(1:ncol(data))) {
-    cc=cor.test(data[,i],var[,x],
+    cc=cor.test(data[,i],clinical[,col],
                 method = methodCC)
     cc1$CC[i]=cc$estimate
     cc1$P.value[i]=cc$p.value
